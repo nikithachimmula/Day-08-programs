050d865cf600bbf7cec6eac9a4def7b6c2426e03
nikithachimmula <91778797+nikithachimmula@users.noreply.github.com>
2021-11-02 16:30:37 +0530

    Add files via upload

---
+++ CompareMethod.java
@@ -0,0 +1,13 @@
+
+public class CompareMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "2";
+		String s2 = "3";
+		System.out.println(s1 + " .compareTo " + s2 + ": " + s1.compareTo(s2));
+
+	}
+
+}
